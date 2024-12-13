1d6a39ed2931eca3798b3a26fa2750305df70ff6
M0000Man <nathanmunro56@gmail.com>
2024-12-13 12:09:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.body.appendChild(renderer.domElement);
 
 // Add a floor
 const floorGeometry = new THREE.PlaneGeometry(100, 100);
-const floorMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
+const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.rotation.x = -Math.PI / 2;
 scene.add(floor);
